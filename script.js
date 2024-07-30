b5601f501105507763210e1c5eea15acfb677dc6
josueramirez1 <josue67ramirez@gmail.com>
2024-07-29 17:23:57 -0700

    adding logic to flow in array
    adding one for player one and adding two for player 2 in array

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
-let gameboard = [];
-
-function createBoard() {
-  let body = document.querySelector("body");
-  let board = document.createElement("div");
-  board.setAttribute("class", "board");
-  body.appendChild(board);
+function Gameboard() {
+  let board = [];
+  let { value, player1, player2 } = marks();
+  let { numFromPlayer1, numFromPlayer2 } = flow();
 
   for (let i = 1; i <= 9; i++) {
-    let tile = document.createElement("div");
-    tile.setAttribute("class", "tile");
-    board.appendChild(tile);
+    board.push(value);
   }
+
+  let updatedBoard = board.map((val, index) => {
+    if (index + 1 === numFromPlayer1) val = player1;
+    if (index + 1 === numFromPlayer2) val = player2;
+
+    return val;
+  });
+
+  return updatedBoard;
 }
 
-createBoard();
+function marks() {
+  let value = 0;
+  let player1 = 1;
+  let player2 = 2;
+  return { value, player1, player2 };
+}
+
+function flow() {
+  let numFromPlayer1 = parseInt(
+    prompt("Player 1: Pick index number from 1 to 9", "")
+  );
+
+  let numFromPlayer2 = parseInt(
+    prompt("Player 2: Pick index number from 1 to 9", "")
+  );
+
+  if (numFromPlayer1 < 0 || numFromPlayer1 > 10) {
+    numFromPlayer1 = parseInt(
+      prompt("Player 1: Pick index number from 1 to 9", "")
+    );
+  }
+
+  if (numFromPlayer2 < 0 || numFromPlayer2 > 10) {
+    numFromPlayer2 = parseInt(
+      prompt("Player 2: Pick index number from 1 to 9", "")
+    );
+  }
+
+  return { numFromPlayer1, numFromPlayer2 };
+}
+
+console.log(Gameboard());
