d199265900b6099fcc9c9144759617d16206251b
josueramirez1 <josue67ramirez@gmail.com>
2024-08-01 23:57:02 -0700

    starting over

--- script.js
+++ script.js
@@ -1,71 +1,32 @@
 function Gameboard() {
   let board = [];
-  let { value, player1, player2 } = marks();
-  let { numFromPlayer1, numFromPlayer2 } = flow();
+  let rows = 3;
+  let cols = 3;
 
-  for (let i = 1; i <= 9; i++) {
-    board.push(value);
-  }
-
-  let updatedBoard = board.map((val, index) => {
-    if (index + 1 === numFromPlayer1) val = player1;
-    if (index + 1 === numFromPlayer2) val = player2;
-
-    return val;
-  });
-
-  return updatedBoard;
-}
-
-function marks() {
-  let value = 0;
-  let player1 = 1;
-  let player2 = 2;
-  return { value, player1, player2 };
-}
-
-function flow() {
-  let numFromPlayer1 = parseInt(
-    prompt("Player 1: Pick index number from 1 to 9", "")
-  );
-
-  let numFromPlayer2 = parseInt(
-    prompt("Player 2: Pick index number from 1 to 9", "")
-  );
-
-  if (numFromPlayer1 < 1 || numFromPlayer1 > 9) {
-    numFromPlayer1 = parseInt(
-      prompt("Player 1: Pick another index number from 1 to 9", "")
-    );
-  }
-
-  if (numFromPlayer2 < 1 || numFromPlayer2 > 9) {
-    numFromPlayer2 = parseInt(
-      prompt("Player 2: Pick another index number from 1 to 9", "")
-    );
-  }
-
-  if (numFromPlayer1 !== numFromPlayer2) {
-    return { numFromPlayer1, numFromPlayer2 };
-  } else {
-    numFromPlayer2 = repeatChoice();
-    return { numFromPlayer1, numFromPlayer2 };
-  }
-
-  function repeatChoice() {
-    let numFromPlayer2 = parseInt(
-      prompt("Player 2: Pick index number from 1 to 9", "")
-    );
-
-    if (numFromPlayer2 < 1 || numFromPlayer2 > 9) {
-      numFromPlayer2 = parseInt(
-        prompt("Player 2: Pick another index number from 1 to 9", "")
-      );
+  let zero = 0;
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < cols; j++) {
+      board[i].push("hello");
     }
-
-    if (numFromPlayer1 === numFromPlayer2) repeatChoice();
-    return numFromPlayer2;
   }
+
+  console.table(board);
 }
 
-console.log(Gameboard());
+Gameboard();
+
+function Players() {
+  let player = [
+    {
+      name: "Player One",
+      marker: "x",
+    },
+    {
+      name: "Player Two",
+      marker: "o",
+    },
+  ];
+}
+
+function gameController() {}
