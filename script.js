fae7d11a9eb863bdd42df971e6ad0f46b07c83f8
josueramirez1 <josue67ramirez@gmail.com>
2024-08-03 16:07:47 -0700

    adding objects and controller
    gives user the ability to add marker to table

--- script.js
+++ script.js
@@ -2,30 +2,21 @@ function Gameboard() {
   let board = [];
   let rows = 3;
   let cols = 3;
-
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < cols; j++) {
-      board[i].push(Cell());
+      board[i].push(Cell().getValue());
     }
   }
   const getBoard = () => board;
-
-  console.table(getBoard());
-
   return { getBoard };
 }
 
-Gameboard();
-
 function Cell() {
-  let blank = "__";
-
-  const getBlank = () => blank;
-
-  const getMarker = () => (player = marker);
-
-  return { getBlank, getMarker };
+  let value = "___";
+  const getValue = () => value;
+  const getMarker = (marker) => (value = marker);
+  return { getValue, getMarker };
 }
 
 function Players() {
@@ -39,6 +30,31 @@ function Players() {
       marker: "o",
     },
   ];
+
+  return player;
 }
 
-function gameController() {}
+function gameController() {
+  // Put the array of player into the player variable to use in the controller object
+
+  let player = Players();
+  // Begin by picking the active player
+  let activePlayer = player[0].marker;
+  // Switch to second player
+  // if (activePlayer !== player[0]) {
+  //   activePlayer = player[1].marker;
+  // }
+
+  // I want the player to pick a cell
+
+  let board = Gameboard().getBoard();
+  console.table(board);
+  let row = parseInt(prompt("Select row from 0 to 2"));
+  let column = parseInt(prompt("Select column from 0 to 2"));
+
+  // then I want the marker to be placed in the cell
+  board[row][column] = Cell().getMarker(activePlayer);
+  console.table(board);
+}
+
+gameController();
