c9f60ef535c668137dbe16b975845e200732e4bb
josueramirez1 <josue67ramirez@gmail.com>
2024-08-03 16:52:27 -0700

    board updated
    board is able to be updated for every round and players take turn updating board.

--- script.js
+++ script.js
@@ -24,10 +24,12 @@ function Players() {
     {
       name: "Player One",
       marker: "x",
+      turn: 0,
     },
     {
       name: "Player Two",
       marker: "o",
+      turn: 0,
     },
   ];
 
@@ -36,25 +38,34 @@ function Players() {
 
 function gameController() {
   // Put the array of player into the player variable to use in the controller object
-
   let player = Players();
+  let board = Gameboard();
   // Begin by picking the active player
-  let activePlayer = player[0].marker;
-  // Switch to second player
-  // if (activePlayer !== player[0]) {
-  //   activePlayer = player[1].marker;
-  // }
+  let activePlayer = player[0];
+  // Switch to second player if first player has taken a turn
+  if (activePlayer.turn > player[1].turn) {
+    activePlayer = player[1];
+  } else activePlayer = player[0];
 
-  // I want the player to pick a cell
+  function playRound() {
+    console.log(activePlayer);
+    // I want the player to pick a cell
+    let row = parseInt(prompt("Select row from 0 to 2"));
+    let column = parseInt(prompt("Select column from 0 to 2"));
 
-  let board = Gameboard().getBoard();
-  console.table(board);
-  let row = parseInt(prompt("Select row from 0 to 2"));
-  let column = parseInt(prompt("Select column from 0 to 2"));
+    // then I want the marker to be placed in the cell
+    board.getBoard()[row][column] = Cell().getMarker(activePlayer.marker);
+    console.table(board.getBoard());
 
-  // then I want the marker to be placed in the cell
-  board[row][column] = Cell().getMarker(activePlayer);
-  console.table(board);
+    activePlayer.turn++;
+    activePlayer = player[1];
+  }
+
+  playRound();
+  playRound();
+  // If there are three markers in a row, declare the winner
+
+  function declareWinner() {}
 }
 
 gameController();
