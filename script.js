106933684b0d4e24829fc4f068ce36f747f98d26
josueramirez1 <josue67ramirez@gmail.com>
2024-08-05 21:14:14 -0700

    completed rough draft of console tic tac toe
    still thinking about making changes but I'm in a good place. Next I have to add constraints

--- script.js
+++ script.js
@@ -43,54 +43,52 @@ function gameController() {
   // Begin by picking the active player
   let activePlayer = player[0];
 
-  // If there are three markers in a row, declare the winner
-
   function playRound() {
     // Switch to second player if first player has taken a turn
     if (activePlayer.turn > player[1].turn) {
       activePlayer = player[1];
     } else activePlayer = player[0];
-
     console.log(activePlayer);
     // I want the player to pick a cell
     let row = parseInt(prompt("Select row from 0 to 2"));
     let column = parseInt(prompt("Select column from 0 to 2"));
-
     // then I want the marker to be placed in the cell
     board.getBoard()[row][column] = Cell().getMarker(activePlayer.marker);
-    console.table(board.getBoard());
-
+    // Add tally to active player
     activePlayer.turn++;
+    // Print updated board
+    console.table(board.getBoard());
+    const result = declareWinner();
+    console.log(result);
   }
 
   function declareWinner() {
     // Get the board
     let updatedBoard = board.getBoard();
-    console.log(updatedBoard);
-
     // Win by diagonal
-    let diagonal = [];
-    updatedBoard.forEach((row) => {
-      if (
-        row[0][0] === "x" ||
-        row[0][2] === "x" ||
-        row[0][0] === "o" ||
-        row[0][2] === "o" ||
-        row[1][1] === "x" ||
-        row[1][1] === "o" ||
-        row[2][0] === "x" ||
-        row[2][2] === "x" ||
-        row[2][0] === "o" ||
-        row[2][2] === "o"
-      ) {
-        diagonal.push(Cell().getMarker(activePlayer.marker));
-        if (diagonal.length === 3) {
-          if (diagonal.every((entry) => entry === "x" || entry === "o")) {
-            console.log("Congratulations! You win by diagonal!");
-          }
+    if (activePlayer === player[0]) {
+      for (let i = 0; i < updatedBoard.length; i++) {
+        if (
+          (updatedBoard[0][0] === "x" || updatedBoard[0][2] === "x") &&
+          updatedBoard[1][1] === "x" &&
+          (updatedBoard[2][0] === "x" || updatedBoard[2][2] === "x")
+        ) {
+          return "Congratulations! You win by diagonal!";
         }
       }
-    });
+    }
+
+    if (activePlayer === player[1]) {
+      for (let i = 0; i < updatedBoard.length; i++) {
+        if (
+          (updatedBoard[0][0] === "o" || updatedBoard[0][2] === "o") &&
+          updatedBoard[1][1] === "o" &&
+          (updatedBoard[2][0] === "o" || updatedBoard[2][2] === "o")
+        ) {
+          return "Congratulations! You win by diagonal!";
+        }
+      }
+    }
 
     // Win by horizontal row
     for (let i = 0; i < updatedBoard.length; i++) {
@@ -99,7 +97,7 @@ function gameController() {
           (cell) => cell === Cell().getMarker(activePlayer.marker)
         )
       ) {
-        console.log("Congratulations! You win by row!");
+        return "Congratulations! You win by row!";
       }
     }
     // Win by vertical row
@@ -108,15 +106,18 @@ function gameController() {
       for (let j = 0; j < 3; j++) {
         column.push(updatedBoard[j][i]);
         if (column.length === 3) {
-          if (column.every((entry) => entry === "x" || entry === "0")) {
-            console.log("Congratulations! You win by column!");
+          if (column.every((entry) => entry === "x")) {
+            return "Congratulations! You win by column!";
+          } else if (column.every((entry) => entry === "o")) {
+            return "Congratulations! You win by column!";
           }
         }
       }
     }
-
-    console.table(updatedBoard);
+    playRound();
   }
+  // initialize the game
+  playRound();
 }
 
 gameController();
