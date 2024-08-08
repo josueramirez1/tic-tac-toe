e4a597096b8bd01114fc85ba916bb16491835573
josueramirez1 <josue67ramirez@gmail.com>
2024-08-07 21:22:50 -0700

    adding function that checks if spot is already taken
    in order to players not to replace spots already taken

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Gameboard() {
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < cols; j++) {
-      board[i].push(Cell());
+      board[i].push(Cell().getValue());
     }
   }
   const getBoard = () => board;
@@ -23,12 +23,10 @@ function Players() {
     {
       name: "Player One",
       marker: "x",
-      turn: 0,
     },
     {
       name: "Player Two",
       marker: "o",
-      turn: 0,
     },
   ];
   return player;
@@ -37,31 +35,40 @@ function gameController() {
   // Put the array of player into the player variable to use in the controller object
   let player = Players();
   let board = Gameboard();
-  // Begin by picking the active player
   let activePlayer = player[0];
-  function playRound() {
-    // Switch to second player if first player has taken a turn
-    if (activePlayer.turn > player[1].turn) {
-      activePlayer = player[1];
-    } else activePlayer = player[0];
-    console.log(activePlayer);
+  // Begin by picking the active player
+  const switchingPlayer = () => {
+    activePlayer = activePlayer === player[0] ? player[1] : player[0];
+  };
+
+  const printNewRound = () => {
+    console.table(board.getBoard());
+  };
+  const getActivePlayer = () => activePlayer;
+
+  const playRound = () => {
+    console.log(`${getActivePlayer().name}: please choose a spot.`);
+
     // I want the player to pick a cell
     let row = parseInt(prompt("Select row from 0 to 2"));
     let column = parseInt(prompt("Select column from 0 to 2"));
+    // Check if the cell already contains a marker
+    // if it does the move is not valid and function is restarted
+    if (board.getBoard()[row][column] !== Cell().getValue()) {
+      playRound();
+    }
     // then I want the marker to be placed in the cell
-    board.getBoard()[row][column] = Cell().getMarker(activePlayer.marker);
+    board.getBoard()[row][column] = Cell().getMarker(getActivePlayer().marker);
     // Add tally to active player
-    activePlayer.turn++;
     // Print updated board
-    console.table(board.getBoard());
-    const result = declareWinner();
-    console.log(result);
-  }
+    printNewRound();
+    console.log(declareWinner());
+  };
 
-  function declareWinner() {
+  const declareWinner = () => {
     // Get the board
     let updatedBoard = board.getBoard();
-    console.log(updatedBoard);
+
     // Win by diagonal
     if (activePlayer === player[0]) {
       for (let i = 0; i < updatedBoard.length; i++) {
@@ -95,7 +102,7 @@ function gameController() {
     for (let i = 0; i < updatedBoard.length; i++) {
       if (
         updatedBoard[i].every(
-          (cell) => cell === Cell().getMarker(activePlayer.marker)
+          (cell) => cell === Cell().getMarker(getActivePlayer().marker)
         )
       ) {
         return "Congratulations! You win by row!";
@@ -137,9 +144,9 @@ function gameController() {
         }
       }
     }
-
+    switchingPlayer();
     playRound();
-  }
+  };
   // initialize the game
   playRound();
 }
