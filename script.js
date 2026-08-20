4101f95d13dfeaa38ab06c3e07ffa4fff4e7e698
josueramirez1 <josue67ramirez@gmail.com>
2026-08-20 16:11:20 -0700

    completed row verification

--- script.js
+++ script.js
@@ -15,8 +15,6 @@ const Gameboard = (() => {
   return { getBoard };
 })();
 
-const gameboard = Gameboard.getBoard();
-
 //Players
 const playerOne = {
   name,
@@ -35,74 +33,103 @@ const getNames = () => {
   playerTwo.name = playerTwoName;
 };
 
-const playerOneTurn = () => {
-  const playerOneTurnNum = parseInt(prompt("From 0 to 8, choose a number"), 10);
+const playerTurn = (gameboard, playerOne, playerTwo, turnCount) => {
+  console.table(gameboard);
+  console.log(turnCount);
+  let activePlayer;
+  turnCount % 2 === 0 ? (activePlayer = playerOne) : (activePlayer = playerTwo);
 
-  if (playerOneTurnNum >= 0 && playerOneTurnNum <= 2) {
-    if (gameboard[0][playerOneTurnNum] !== "") {
+  const activePlayerNum = parseInt(
+    prompt(`${activePlayer.name}: From 0 to 8, choose a number`),
+    10,
+  );
+
+  if (activePlayerNum >= 0 && activePlayerNum <= 2) {
+    if (gameboard[0][activePlayerNum] !== "") {
       alert("Spot taken. Pick another space");
-      playerOneTurn();
+      playerTurn(gameboard, playerOne, playerTwo, turnCount);
       return;
     }
-    gameboard[0][playerOneTurnNum] = playerOne.marker;
+    gameboard[0][activePlayerNum] = activePlayer.marker;
+    checkWinner(gameboard, activePlayer);
+    return ++turnCount;
   }
 
-  if (playerOneTurnNum >= 3 && playerOneTurnNum <= 5) {
-    if (gameboard[1][playerOneTurnNum - 3] !== "") {
+  if (activePlayerNum >= 3 && activePlayerNum <= 5) {
+    if (gameboard[1][activePlayerNum - 3] !== "") {
       alert("Spot taken 2. Pick another space");
-      playerOneTurn();
+      playerTurn(gameboard, playerOne, playerTwo, turnCount);
       return;
     }
-    gameboard[1][playerOneTurnNum - 3] = playerOne.marker;
+    gameboard[1][activePlayerNum - 3] = activePlayer.marker;
+    checkWinner(gameboard, activePlayer);
+    return ++turnCount;
   }
 
-  if (playerOneTurnNum >= 6 && playerOneTurnNum <= 8) {
-    if (gameboard[2][playerOneTurnNum - 6] !== "") {
+  if (activePlayerNum >= 6 && activePlayerNum <= 8) {
+    if (gameboard[2][activePlayerNum - 6] !== "") {
       alert("Spot taken 3. Pick another space");
-      playerOneTurn();
+      playerTurn(gameboard, playerOne, playerTwo, turnCount);
       return;
     }
-    gameboard[2][playerOneTurnNum - 6] = playerOne.marker;
+    gameboard[2][activePlayerNum - 6] = activePlayer.marker;
+    checkWinner(gameboard, activePlayer);
+    return ++turnCount;
   }
 };
 
-const playerTwoTurn = () => {
-  const playerTwoTurnNum = parseInt(prompt("From 0 to 8, choose a number"), 10);
+const playRound = (gameboard, turnCount) => {
+  playerTurn(gameboard, playerOne, playerTwo, turnCount);
+  console.log(turnCount);
+  //if conditions haven't been met keep playing
+  playRound(gameboard, turnCount);
+};
 
-  if (playerTwoTurnNum >= 0 && playerTwoTurnNum <= 2) {
-    if (gameboard[0][playerTwoTurnNum] !== "") {
-      alert("Spot taken. Pick another space");
-      playerTwoTurn();
-      return;
-    }
-    gameboard[0][playerTwoTurnNum] = playerTwo.marker;
+const checkRowWin = (gameboard, activePlayer) => {
+  const [columnArrOne, columnArrTwo, columnArrThree] = gameboard;
+  // This condition checks winners for row wins
+  if (
+    columnArrOne.every((space) => space === activePlayer.marker) ||
+    columnArrTwo.every((space) => space === activePlayer.marker) ||
+    columnArrThree.every((space) => space === activePlayer.marker)
+  ) {
+    endGame(activePlayer.name, gameboard);
+    return;
   }
+};
 
-  if (playerTwoTurnNum >= 3 && playerTwoTurnNum <= 5) {
-    if (gameboard[1][playerTwoTurnNum - 3] !== "") {
-      alert("Spot taken 2. Pick another space");
-      playerTwoTurn();
-      return;
-    }
-    gameboard[1][playerTwoTurnNum - 3] = playerTwo.marker;
-  }
+const checkColumnWin = (gameboard) => {
+  gameboard.forEach((columnArr) => {
+    columnArr.forEach((space, index) => {
+      if (
+        (index === 0 && space === playerOne.marker) ||
+        (index === 1 && space === playerOne.marker) ||
+        (index === 2 && space === playerOne.marker)
+      ) {
+        console.log("winner");
+        endGame(gameboard);
+      }
+    });
+  });
+};
 
-  if (playerTwoTurnNum >= 6 && playerTwoTurnNum <= 8) {
-    if (gameboard[2][playerTwoTurnNum - 6] !== "") {
-      alert("Spot taken 3. Pick another space");
-      playerTwoTurn();
-      return;
-    }
-    gameboard[2][playerTwoTurnNum - 6] = playerTwo.marker;
-  }
+const endGame = (playerName, gameboard) => {
+  debugger;
+  alert(`${playerName} wins!`);
+  console.table(gameboard);
+  return;
+};
+
+const checkWinner = (gameboard, activePlayer) => {
+  checkRowWin(gameboard, activePlayer);
+  // checkColumnWin(gameboard);
 };
 
 const playGame = () => {
-  //Names
-  //   getNames();
-  playerOneTurn();
-  playerTwoTurn();
-  console.log(gameboard);
+  const gameboard = Gameboard.getBoard();
+  let turnCount = 0;
+  getNames();
+  playRound(gameboard, turnCount);
 };
 
 playGame();
